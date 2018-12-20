29f33720d5be15948d2147714ee3cc6027e05095
Carlos Villavicencio <cvillavicenc@u-bordeaux.fr>
2018-12-21 00:03:58 +0100

    parsing command-line arguments for the script

--- main.py
+++ main.py
@@ -3,36 +3,55 @@ import pygame
 import sys
 import json
 import time
+import argparse
 
 from board import *
 from greedy import *
 from brute_force import *
 
 
-if (len(sys.argv) < 3) :
-    sys.exit("Usage: " + sys.argv[0] + " <problem.json> <mode>")
 
-start_time = time.time()
+parser = argparse.ArgumentParser(description='Robocup (Projet d\'Algorithmique Appliquée)')
 
-problem_path = sys.argv[1]
-mode = sys.argv[2]
+parser.add_argument('-g','--graph', action='store_true', help='display graphical result with players', default = True )
+parser.add_argument('-t', '--time', action="store_true", help='shows the algorithm time (without graphical view)', default=False )
+parser.add_argument('-a', '--algo', default="greedy", choices=['greedy', 'brute'], help="search algorithm")
+parser.add_argument('filename', help="Json file problem")
+
+args = parser.parse_args()
+
 
 #read problem
-with open(problem_path) as problem_file:
+with open(args.filename) as problem_file:
     problem = Problem(json.load(problem_file))
 
 #create graph
+if args.time:
+    start_time = time.time()
 G = Graph(problem)
+if args.time:
+    print("--Graph construction time: %s seconds --" % (time.time() - start_time))
 
-#initialize greedy algorithm
-brute = Greedy(G)
-#brute = BruteForce(G)
+
+#initialize algorithm
+algo = None
+if args.algo == 'greedy':
+    algo = Greedy(G)
+else:
+    algo = BruteForce(G)
 
 
 #search best positions
 positions = []
-for v in brute.execute():
+
+if args.time:
+    start_time = time.time()
+
+for v in algo.execute():
     positions.append(v.getPosition())
+
+if args.time:
+    print("--Algorithm time: %s seconds --" % (time.time() - start_time))
 
 
 #dump(save) best positions
@@ -41,16 +60,12 @@ data['defenders'] = positions
 with open('configs/solution.json', 'w') as outfile:
     json.dump(data, outfile)
 
-
 #display result
-if mode=="-g":
+if not args.time:
     #read the solution
     with open('configs/solution.json') as solution_file:
         solution = Solution(json.load(solution_file))
     b = Board(problem, solution)
     b.run()
-
-if mode=="-p":
-    print("-- %s seconds --" % (time.time() - start_time))
 
 sys.exit()
